c47ecf21251b8a0a4117e3a3a030ae282cd6e827
Andre-JierongLuo <rjluo17@gmail.com>
2014-06-04 10:49:02 -0400

    setup

--- SnakeGame/MyScene.m
+++ SnakeGame/MyScene.m
@@ -8,29 +8,60 @@
 
 #import "MyScene.h"
 
-@implementation MyScene
+@implementation MyScene{
+    NSMutableArray* Body;
+    SKSpriteNode* Food;
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
+        /*
         SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
         
         myLabel.text = @"Hello, World!";
         myLabel.fontSize = 30;
         myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));
-        
         [self addChild:myLabel];
+        */
+        NSLog(@"****** initWithSize ****** %@ ****** \n", NSStringFromCGSize(size));
+        
+        if([self children] != nil){
+            [self removeAllChildren];
+        }
+        [self startGame];
     }
     return self;
 }
 
+-(void)startGame{
+    NSLog(@"***** startGame ****** \n");
+    
+    // initialized snake
+    Body = [[NSMutableArray alloc]init];
+    
+    for(int i = 0; i < 3; i++){
+        SKSpriteNode* node = [[SKSpriteNode alloc] initWithImageNamed:@"body.png"];
+        [node setSize:CGSizeMake(10, 10)];
+        [node setPosition:CGPointMake(5, 12*i+30)];
+        [self addChild:node];
+        [Body addObject:node];
+    }
+    
+    Food = [[SKSpriteNode alloc] initWithImageNamed:@"food.png"];
+    [Food setSize:CGSizeMake(10, 10)];
+    [Food setPosition:CGPointMake(100, 200)];
+    [self addChild:Food];
+    
+}
+
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
+    /*
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
@@ -44,6 +75,7 @@
         
         [self addChild:sprite];
     }
+     */
 }
 
 -(void)update:(CFTimeInterval)currentTime {
